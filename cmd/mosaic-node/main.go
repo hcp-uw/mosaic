bf68e6c8b4b3d77b715e1a67b4e8340aac6742cb
gavin <gsman707@gmail.com>
2025-10-30 02:02:03 -0700

    encoding and decoding shards works

--- cmd/mosaic-node/main.go
+++ cmd/mosaic-node/main.go
@@ -2,12 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
-	//"github.com/hcp-uw/mosaic/internal/encoding"
+	"github.com/hcp-uw/mosaic/internal/encoding"
 )
 
-
 func main() {
-	
+	//for testing purposes rn
+	file, err := os.ReadFile("./files/pic.jpg")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileSize := len(file)
+
+	encoder, err := encoding.NewEncoder(8, 4, "./files")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := encoder.EncodeFile("/pic.jpg")
+
+	// oh no 3 shards are missing!
+
+	data[2] = nil
+	data[4] = nil
+	data[8] = nil
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = encoder.DecodeShards(data, fileSize)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("welcome to mosaic")
 }
--- internal/encoding/reedSolomon.go
+++ internal/encoding/reedSolomon.go
@@ -1,159 +1,142 @@
 package encoding
 
 import (
+	"bytes"
 	"errors"
 	"log"
-	"math"
+	"net/http"
 	"os"
+
+	"github.com/klauspost/reedsolomon"
 )
 
-var exp [512]byte
-var logTable [256]byte
+type Encoder struct {
+	encoder reedsolomon.Encoder
+	shards  int
+	parity  int
 
-const primitive = byte(0x1D)
-
-func init() {
-	initGFTables()
-	log.Println(exp)
+	dirPath string
 }
 
-// ts function is the main encode function -- will return a list of files which are the shards
-func RSEncode(path string, shards int, parity int) ([]*os.File, error) {
+func NewEncoder(dataShards int, parityShards int, path string) (*Encoder, error) {
 
-	fileToEncode, err := os.ReadFile(path)
+	if dataShards <= 0 || parityShards <= 0 {
+		return nil, errors.New("Shard counts have to be greater than or equal to 0")
+	}
+
+	encoder, err := reedsolomon.New(dataShards, parityShards)
+
 	if err != nil {
 		return nil, err
 	}
 
-	brokenFile, err := fileSplit(fileToEncode, shards)
+	info, err := os.Stat(path)
+
+	if os.IsNotExist(err) {
+		return nil, errors.New("This directory doesn't exist")
+	}
+
+	if !info.IsDir() {
+		return nil, errors.New("Path exists but ts is not a directory")
+	}
+
+	newEncoder := &Encoder{
+		encoder: encoder,
+		shards:  dataShards,
+		parity:  parityShards,
+
+		dirPath: path,
+	}
+
+	return newEncoder, nil
+}
+
+// ts function encodes the file while having all of it stored in memory - sheeesh (will fix)
+func (e *Encoder) EncodeFile(relativeFilePath string) ([][]byte, error) {
+
+	fileToEncode, err := os.ReadFile(e.dirPath + relativeFilePath)
 	if err != nil {
 		return nil, err
 	}
-	log.Println(brokenFile)
 
-	return nil, errors.New("temp")
-
-}
-
-// splits file into k shards
-func fileSplit(file []byte, shards int) ([][]byte, error) {
-
-	if shards <= 0 {
-		return nil, errors.New("shard count has to be greater than 0")
+	splitFile, err := e.encoder.Split(fileToEncode)
+	if err != nil {
+		return nil, err
 	}
 
-	if len(file) == 0 {
-		return nil, errors.New("File length cannot be 0")
-	}
-
-	shardSize := int(math.Ceil(float64(len(file)) / float64(shards)))
-
-	var splitFile [][]byte
-
-	for i := range shards {
-		currentChunkStart := shardSize * i
-		currentChunkEnd := min(currentChunkStart+shardSize, len(file))
-
-		currentChunk := file[currentChunkStart:currentChunkEnd]
-		splitFile = append(splitFile, currentChunk)
+	err = e.encoder.Encode(splitFile)
+	if err != nil {
+		return nil, err
 	}
 
 	return splitFile, nil
 }
 
-// for RS encoding algebraic operations need to be definined in a finite field
-// basically if two bytes are added together they dont go over the byte limit or 255
-// for some reason finite field is notated to GF (galois field)
+// right now ts has to remember file length will fix later
+func (e *Encoder) DecodeShards(shards [][]byte, fileLength int) error {
+	if len(shards) < e.shards {
+		return errors.New("Not enough data to reconstruct")
+	}
+	data := make([][]byte, e.shards+e.parity)
 
-// adds a gf
-func gfAdd(a byte, b byte) byte {
-	return a ^ b
+	// Copy existing shards
+	for i := range shards {
+		data[i] = make([]byte, len(shards[i]))
+		copy(data[i], shards[i])
+	}
+
+	// Initialize empty parity shards
+	for i := len(shards); i < e.shards+e.parity; i++ {
+		data[i] = make([]byte, len(shards[0]))
+	}
+
+	err := e.encoder.ReconstructData(data)
+
+	if err != nil {
+		return err
+	}
+
+	var buf bytes.Buffer
+
+	err = e.encoder.Join(&buf, data, fileLength)
+	if err != nil {
+		log.Println("here")
+		return err
+	}
+
+	joinedBytes := buf.Bytes()
+
+	fileExtension := detectExtension(joinedBytes)
+	outFile, err := os.Create("output_file" + fileExtension)
+	if err != nil {
+		return err
+	}
+
+	defer outFile.Close()
+
+	_, err = outFile.Write(joinedBytes)
+
+	log.Println("File written to ./output_file")
+
+	return err
 }
 
-// multiplies a gf
-func gfMultiply(a byte, b byte) byte {
-	if a == 0 || b == 0 {
-		return 0
+func detectExtension(data []byte) string {
+	if len(data) > 512 {
+		data = data[:512] // only need first 512 bytes
 	}
+	mimeType := http.DetectContentType(data)
 
-	logA := int(logTable[a])
-	logB := int(logTable[b])
-	log.Printf("logA: %d, logB: %d\n", logA, logB)
-
-	sumLog := logA + logB
-
-	return exp[sumLog]
-}
-
-// divides a gf not quite sure ts is necessary yet
-func gfDivid(a byte, b byte) (byte, error) {
-	if b == 0 {
-		return 0, errors.New("divide by zero lookin ahh")
+	switch mimeType {
+	case "image/png":
+		return ".png"
+	case "image/jpeg":
+		return ".jpg"
+	case "application/pdf":
+		return ".pdf"
+	case "text/plain; charset=utf-8":
+		return ".txt"
+	default:
+		return ".bin" // fallback
 	}
-
-	if a == 0 {
-		return 0, nil
-	}
-
-	logA := int(logTable[a])
-	logB := int(logTable[b])
-
-	difLog := logA - logB
-
-	if difLog < 0 {
-		difLog += 255
-	}
-
-	return exp[difLog], nil
-}
-
-// when you raise gf to a power
-func gfPow(a byte, p int) byte {
-	if p == 0 {
-		return 1
-	}
-
-	if a == 0 {
-		return 0
-	}
-
-	exp := (p * int(logTable[a])) % 255
-
-	return byte(exp)
-}
-
-// initializes gf tables to do super uber fast multiplication
-func initGFTables() {
-	x := 1
-
-	for i := range 255 {
-		exp[i] = byte(x)
-		logTable[byte(x)] = byte(i)
-
-		x <<= 1
-
-		if (x & 0x100) != 0 {
-			x ^= int(primitive)
-		}
-	}
-
-	logTable[0] = 255
-
-	for i := 255; i < 512; i++ {
-		exp[i] = exp[i-255]
-	}
-}
-
-// creates a vandermonde matrix in GF zone
-func createMatrix(baseShards int, parityShards int) [][]byte {
-	matrix := make([][]byte, baseShards+parityShards)
-
-	for row := 0; row < baseShards+parityShards; row++ {
-		matrix[row] = make([]byte, baseShards)
-		for col := range baseShards {
-			matrix[row][col] = gfPow(byte(row+1), col)
-		}
-	}
-
-	return matrix
 }
